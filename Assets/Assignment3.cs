6cae6aa15ea5b64cda78ac02b17c5950c0375781
wanderingkitty <alieri0421@skola.goteborg.se>
2021-09-22 15:42:10 +0200

    circle press button movement

--- Assets/Assignment3.cs
+++ Assets/Assignment3.cs
@@ -7,44 +7,32 @@ public class Assignment3 : ProcessingLite.GP21
 {
     
     public Vector2 circlePosition;
-    public float diameter = 2;
-    public Vector2 direction;
+    //float diameter = 1f;
+    //public Vector2 direction;
     
 
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        circlePosition = new Vector2(Width / 2, Height / 2); 
     }
     // Update is called once per frame
     void Update()
     {
         Background(0);
-        circlePosition.x = MouseX;
-        circlePosition.y = MouseY;
-        
-        
+        Circle(circlePosition.x, circlePosition.y, 1);
 
         if (Input.GetMouseButtonDown(0))
         {
             circlePosition.x = MouseX;
             circlePosition.y = MouseY;
-        }
-
-        if (Input.GetMouseButton(0))
-        {
-            Line(circlePosition.x, circlePosition.y, MouseX, MouseY);
-        }
-
-        else
-        {
-
-            Circle(circlePosition.x, circlePosition.y, diameter);
-            direction = new Vector2(MouseX - circlePosition.x, MouseY - circlePosition.y);
+        } 
             
 
-        }
+
+
+
     }
 
 
